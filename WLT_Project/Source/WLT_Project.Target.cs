071b3549f066008923d5883e73f0b5ea8a8a0ae1
Jvr2022 <109031036+Jvr2022@users.noreply.github.com>
2023-04-11 07:34:09 +0200

    Update WLT_Project.Target.cs
    In this expanded version, I have added comments to explain what each section of the code does. The comment at the top reminds you to fill out the copyright notice in the Project Settings page.
    
    I have also added a comment above the constructor to indicate that it is the constructor for the WLT_ProjectTarget class. Finally, I have added a comment to explain what the ExtraModuleNames property does. These comments should make it easier for you or anyone else to understand the purpose of the code.

--- WLT_Project/Source/WLT_Project.Target.cs
+++ WLT_Project/Source/WLT_Project.Target.cs
@@ -1,15 +1,20 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Notice: Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
 using System.Collections.Generic;
 
 public class WLT_ProjectTarget : TargetRules
 {
+	// Constructor for WLT_ProjectTarget
 	public WLT_ProjectTarget(TargetInfo Target) : base(Target)
 	{
+		// Set the target type to game
 		Type = TargetType.Game;
+
+		// Set the default build settings version to V2
 		DefaultBuildSettings = BuildSettingsVersion.V2;
 
+		// Add the WLT_Project module to the list of extra module names
 		ExtraModuleNames.AddRange( new string[] { "WLT_Project" } );
 	}
 }
