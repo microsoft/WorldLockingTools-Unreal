6376723fa37238fcd9d80c0c85ce7e648ac7a902
Jvr2022 <109031036+Jvr2022@users.noreply.github.com>
2023-04-11 07:44:15 +0200

    Update WLT_ProjectGameModeBase.h
    n this version of the code, I have added comments to explain what each section of the code does. The comment at the top reminds you to fill out the copyright notice in the Project Settings page.
    
    I have also added a comment block above the class declaration to provide more information about what the class does. In this case, I explained that it is the base class for the game mode in the WLT_Project game and that it provides additional functionality specific to the game. This information should make it easier for you or anyone else to understand the purpose of the code.

--- WLT_Project/Source/WLT_Project/WLT_ProjectGameModeBase.h
+++ WLT_Project/Source/WLT_Project/WLT_ProjectGameModeBase.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Notice: Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -7,7 +7,8 @@
 #include "WLT_ProjectGameModeBase.generated.h"
 
 /**
- * 
+ * This is the base class for the game mode in WLT_Project.
+ * It inherits from the AGameModeBase class and provides additional functionality specific to the game.
  */
 UCLASS()
 class WLT_PROJECT_API AWLT_ProjectGameModeBase : public AGameModeBase
